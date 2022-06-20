eeec749893ba1fccbf914594633e3c6ed5cce67b
Changmin Lucas Lee <67988060+lucathree@users.noreply.github.com>
2022-06-20 17:29:17 +0900

    Update clock.js

--- lofi-todo/js/clock.js
+++ lofi-todo/js/clock.js
@@ -3,7 +3,7 @@ const clock = document.querySelector("#clock");
 function getClock() {
   const date = new Date();
   const year = String(date.getFullYear());
-  const month = String(date.getMonth()).padStart(2, "0");
+  const month = String((date.getMonth()+1)).padStart(2, "0");
   const singleDate = String(date.getDate()).padStart(2, "0");
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
